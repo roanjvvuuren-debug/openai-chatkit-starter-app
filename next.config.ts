54c9526859a140dfc66780565ed085b88ce47d90
roanjvvuuren-debug <roanjansevanvuuren14@gmail.com>
2025-10-22 20:59:30 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,24 @@ const nextConfig: NextConfig = {
     };
     return config;
   },
+
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: "frame-ancestors 'self' https://framer.com https://*.framer.website;",
+          },
+          {
+            key: "X-Frame-Options",
+            value: "ALLOW-FROM https://framer.com",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
